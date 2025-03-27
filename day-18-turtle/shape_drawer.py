ce0fa0480ab334aaf35a0fc26deec6417d7bb92b
Soressa Alemayehu <soressaalemayehu21@gmail.com>
2025-03-27 08:34:16 +0300

    Create shape_drawer.py

---
+++ day-18-turtle/shape_drawer.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+from random import choice
+
+
+
+colors = ['red', 'blue', 'green', 'brown', 'orange']
+tim = Turtle()
+
+n = 3
+while n < 11:
+    for _ in range(n):
+        tim.right(360/n)
+        tim.forward(90)
+    tim.color(choice(colors))
+    n += 1
+
+
+screen = Screen()
+screen.exitonclick()
