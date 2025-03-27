1398c656a60fc4a1277acac86ee11bb62c7267d7
Soressa Alemayehu <soressaalemayehu21@gmail.com>
2025-03-27 08:33:53 +0300

    Update main.py

--- day-17-quizz/main.py
+++ day-17-quizz/main.py
@@ -9,5 +9,9 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-    
-QuizBrain(question_bank).nex_question()
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("you have completed the quiz")
+print(f"you have got {quiz.score}/{quiz.question_number}")
