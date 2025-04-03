52dd5ceda8be643194c2d72a1b33da952e2cb133
Soressa Alemayehu <soressaalemayehu21@gmail.com>
2025-04-03 22:46:23 +0300

    to do

---
+++ day-20/main.py
@@ -0,0 +1,23 @@
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("my snake game")
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+
+for position in starting_positions:
+    snake = Turtle("square")
+    snake.color("white")
+    snake.goto(position)
+
+
+
+
+
+
+
+
+screen.exitonclick()
